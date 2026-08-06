066679c39fd3474b6f60d107b247d2561656d520
Westruva <waynesifiso71@gmail.com>
2026-08-06 14:54:49 +0000

    Solution: for tempConversion.js
    I have modified the functions to round the results to one decimal place.
    For convertToCelsius, I added a line to round the calculated Celsius
    value before returning it. Similarly, for convertToFahrenheit, I added a
    line to round the calculated Fahrenheit value before returning it.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) { 
+    const celsius = (tempFahrenheit - 32) * 5/9;
+    return Math.round(celsius * 10) / 10; // Round to one decimal place
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+    const fahrenheit = (tempCelsius * 9/5) + 32;
+    return Math.round(fahrenheit * 10) / 10; // Round to one decimal place
 };
 
 // Do not edit below this line
