e674715d669742fbd1ca973ebf2c6209a48e8945
Westruva <waynesifiso71@gmail.com>
2026-08-06 14:35:18 +0000

    Solution for 08_removeFromArray and 09_sumAll

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...theArgs) {
+    return array.filter(item => !theArgs.includes(item));
+    //takes the item --item-- and compares it to the array of the args
+    //if the item is included, itreturns true which will then be converted into false by the NOT logic
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a <= 0 || b <= 0) {
+        return "ERROR"
+    } else {
+        let array = [a, b].sort((a, b) => a - b);
+        let rangeArray = []
+        for (let i = array[0]; i <= array[1]; i++) {
+            rangeArray.push(i);
+        };
+        return rangeArray.reduce((accumulator, currentvalue) => accumulator + currentvalue);
+    };
 
-};
+}; 
 
 // Do not edit below this line
 module.exports = sumAll;
